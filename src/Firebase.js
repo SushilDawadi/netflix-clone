8d8aa7511816979e622cded715b0136ae96a4df9
Sushil <sushildawadi14@gmail.com>
2023-08-19 21:34:38 +0545

    initail commit

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDF-PWEviBq-T2W4y08UxbR5VUtBxJy8Y4",
+  authDomain: "netflix-clone-aa873.firebaseapp.com",
+  projectId: "netflix-clone-aa873",
+  storageBucket: "netflix-clone-aa873.appspot.com",
+  messagingSenderId: "902495376595",
+  appId: "1:902495376595:web:a26ae773408f978dcab026",
+};
