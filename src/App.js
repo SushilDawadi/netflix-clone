5e2ea5567df2d3a2d847155bd548c58e98ba9802
Sushil <sushildawadi14@gmail.com>
2023-08-31 18:35:58 +0545

    minnor changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginScreen from "./components/LoginScreen";
 function App() {
-  
+  const user = null;
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<HomeScreen />} />
-        </Routes>
-      </Router>
+      {!user ? (
+        <LoginScreen />
+      ) : (
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/LoginScreen.css";
+import logo from "../assets/logo.png";
+const LoginScreen = () => {
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen_background">
+        <img className="loginScreen_logo" src={logo} alt="backgroundImage" />
+        <button className="loginScreen_button">Sign In</button>
+        <div className="loginScreen_gradient" />
+      </div>
+      <div className="loginScreen_body">
+        <>
+          <h1>Unlimited flims ,TV programmes and more. </h1>
+          <h2>Watch anywhere.Cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="loginScreen_input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button className="loginScreen_getStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
